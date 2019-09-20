bc7f9b1c196938dedb5b26c3efed8e6a3e745b19
iobasa <i_obasa@fanshaweonline.ca>
2019-09-20 18:47:31 -0400

    adjusted the site so everything is okay

---
+++ js/modules/DataObject.js
@@ -0,0 +1,19 @@
+const Team = {
+    Ife : {
+    avatar: "member-ife.png"
+    },
+
+    Derrik : {
+    avatar: "member-derrik.png"
+    },
+
+    Dana : {
+    avatar: "member-dana.png"
+    },
+
+    Edi : {
+    avatar: "member-edi.png"
+    }
+}
+
+export default Team;
